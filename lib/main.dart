6946df662b5881eccd540260ae39a06902c8ddd4
MasayaYamada <masaya.yamada.stripes@gmail.com>
2020-03-03 11:27:44 +0900

    change source codes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class Pavlova extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final stars = Row(
-      mainAxisAlignment: MainAxisSize.min,
+      mainAxisSize: MainAxisSize.min,
       children: [
         Icon(Icons.star, color: Colors.green[500]),
         Icon(Icons.star, color: Colors.green[500]),
@@ -52,6 +52,68 @@ class Pavlova extends StatelessWidget {
       letterSpacing: 0.5,
       fontSize: 18,
       height: 2,
+    );
+
+    //DefaultTextStyle.merge()  を使用する事で、style に指定した要素に
+    // child いかにあるテキスト要素に反映する事が出来ます。
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Text('PREP:'),
+                Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                Text('COOK:'),
+                Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                Text('FEEDS'),
+                Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+    final leftColum = Container(
+      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [
+          Text('titleText'),
+          Text('subTitle'),
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+    final sample = Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 340,
+          child: leftColum,
+        ),
+        Image.asset('images/catimage.jpg'),
+      ],
+    );
+
+    return Scaffold(
+      body: Center(child: sample),
     );
   }
 }
